a4f57dc406a420f1fbe6c320352ec5d79656409b
Kushagra Kalash <kushagrakdev@gmail.com>
2018-06-30 14:49:21 +0530

    Can now detect faces!

--- detectFaces.py
+++ detectFaces.py
@@ -1,7 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import time
-plt.ioff()
+plt.ion()
 
 def makeRGB(img):
     return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -10,6 +10,11 @@ test1 = cv2.imread("dataset\\neutral\\S005_001_00000001.png")
 if test1.any() == None:
     raise Exception("could not load image !")
 gray_img = cv2.cvtColor(test1, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Test Image", gray_img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+haar_face_cascade = cv2.CascadeClassifier("cascade-classifiers\haarcascade_frontalface_alt.xml")
+faces = haar_face_cascade.detectMultiScale(gray_img, scaleFactor=1.1, minNeighbors=5)
+print(faces)
+for (x, y, w, h) in faces:
+    cv2.rectangle(test1, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.imshow('Test Image', makeRGB(test1))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
